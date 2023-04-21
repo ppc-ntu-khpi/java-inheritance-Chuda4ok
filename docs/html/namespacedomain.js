3fd87909f954d3b468ea9677e43f13cbd604aeee
Umanets Oleksandr <98989928+Chuda4ok@users.noreply.github.com>
2023-04-21 18:03:57 +0300

    Add files via upload

---
+++ docs/html/namespacedomain.js
@@ -0,0 +1,6 @@
+var namespacedomain =
+[
+    [ "Animal", "classdomain_1_1_animal.html", "classdomain_1_1_animal" ],
+    [ "Dog", "classdomain_1_1_dog.html", "classdomain_1_1_dog" ],
+    [ "Predator", "classdomain_1_1_predator.html", "classdomain_1_1_predator" ]
+];
